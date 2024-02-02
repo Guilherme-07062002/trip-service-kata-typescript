81e44bbe68a4dd8ac9eafc821b66afc4423428cc
Guilherme-07062002 <guilherme762002@gmail.com>
2024-02-02 13:51:46 -0300

    test: trip service

--- src/trip/TripService.ts
+++ src/trip/TripService.ts
@@ -5,26 +5,32 @@ import Trip from "./Trip";
 import TripDAO from "./TripDAO";
 
 export default class TripService {
-    public getTripsByUser(user: User): Trip[] {
-        let tripList: Trip[] = [];
-        const loggedUser: User = UserSession.getLoggedUser();
-        let isFriend: boolean = false;
+  public getTripsByUser(user: User): Trip[] {
+    let tripList: Trip[] = [];
+    const loggedUser: User = this.getLoggedUser();
+    let isFriend: boolean = false;
 
-        if (loggedUser != null) {
-            for (const friend of user.getFriends()) {
-                if (friend === loggedUser) {
-                    isFriend = true;
-                    break;
-                }
-            }
-
-            if (isFriend) {
-                tripList = TripDAO.findTripsByUser(user);
-            }
-
-            return tripList;
-        } else {
-            throw new UserNotLoggedInException();
+    if (loggedUser != null) {
+      for (const friend of user.getFriends()) {
+        if (friend === loggedUser) {
+          isFriend = true;
+          break;
         }
+      }
+
+      if (isFriend) {
+        tripList = this.findTripsByUser(user);
+      }
+
+      return tripList;
+    } else {
+      throw new UserNotLoggedInException();
     }
+  }
+  protected getLoggedUser(): User | null {
+    return UserSession.getLoggedUser();
+  }
+  protected findTripsByUser(user: User): Trip[] {
+    return TripDAO.findTripsByUser(user);
+  }
 }
--- test/TripServiceShould.test.ts
+++ test/TripServiceShould.test.ts
@@ -1,7 +1,68 @@
 import "jest";
+import TripService from "../src/trip/TripService";
+import User from "../src/user/User";
+import Trip from "../src/trip/Trip";
+import UserNotLoggedInException from "../src/exception/UserNotLoggedInException";
 
-describe("TripServiceShould", () => {
-    it("...", () => {
-        expect(4 + 4).toBe(9);
-    });
+const makeSut = () => {
+  class TestableTripService extends TripService {
+    public getLoggedUser(): any {
+      return;
+    }
+    public findTripsByUser(user: User): any {
+      return user.getTrips();
+    }
+  }
+  const fakeTripService = new TestableTripService();
+
+  return { fakeTripService };
+};
+
+describe("testing trip service", () => {
+  it("should list trips of an user", () => {
+    const { fakeTripService } = makeSut();
+    const user1 = new User();
+    const user2 = new User();
+    const trip = new Trip();
+    user1.addFriend(user2);
+    user1.addTrip(trip);
+    fakeTripService.getLoggedUser = jest.fn(() => user2);
+    const trips = fakeTripService.getTripsByUser(user1);
+    expect(trips).toEqual([trip]);
+  });
+
+  test("should throw an error if user is not logged in", () => {
+    const { fakeTripService } = makeSut();
+    const user1 = new User();
+    const user2 = new User();
+    user1.addFriend(user2);
+    fakeTripService.getLoggedUser = jest.fn(() => null);
+    expect(() => fakeTripService.getTripsByUser(user1)).toThrow(
+      UserNotLoggedInException
+    );
+  });
+
+  test("should not list trips because no one of the user friends is logged in", () => {
+    const { fakeTripService } = makeSut();
+    const user1 = new User();
+    const user2 = new User();
+    const user3 = new User();
+    const trip = new Trip();
+    user1.addFriend(user2);
+    user1.addTrip(trip);
+    fakeTripService.getLoggedUser = jest.fn(() => user3);
+    const trips = fakeTripService.getTripsByUser(user1);
+    expect(trips).toEqual([]);
+  });
+
+  test("should not list trips because user has no friends", () => {
+    const { fakeTripService } = makeSut();
+    const user1 = new User();
+    const user2 = new User();
+    const trip = new Trip();
+    user1.addTrip(trip);
+    fakeTripService.getLoggedUser = jest.fn(() => user2);
+    const trips = fakeTripService.getTripsByUser(user1);
+    expect(trips).toEqual([]);
+  });
 });
